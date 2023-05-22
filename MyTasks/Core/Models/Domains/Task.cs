58cba4fddfc4409852e3b7b558e66cb37c0d7f44
Robert Wolniewicz <robert_wol@wp.pl>
2023-05-22 23:06:34 +0200

    Add project files.

---
+++ MyTasks/Core/Models/Domains/Task.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTasks.Core.Models.Domains
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pole Kategoria jest wymagane")]
+        [Display(Name = "Kategoria")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Pole Tytuł jest wymagane")]
+        [MaxLength(50)]
+        [Display(Name ="Tytuł")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Pole Opis jest wymagane")]
+        [MaxLength(250)]
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Termin")]
+        public DateTime? Term { get; set; }
+
+        [Display(Name = "Zrealizowane")]
+        public bool IsExecuted { get; set; }
+
+        public string UserId { get; set; }
+
+        public Category Category { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
